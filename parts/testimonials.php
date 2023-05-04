74f1673a35e9ec39bcb782cbc54a688530cb31c4
Yehor Savchuk <95549799+YehorSk@users.noreply.github.com>
2023-05-04 21:40:21 +0200

    Included Testimonials

---
+++ parts/testimonials.php
@@ -0,0 +1,67 @@
+<section class="testimonial-section" id="testimonial-section">
+    <h1 class="name grid-span-3">Testimonials</h1>
+    <article class="testimonial bg-one grid-span-2">
+        <div class="flex">
+            <div>
+                <img  src="./img/image-daniel.jpg" alt="daniel clifford">
+            </div>
+            <div>
+                <h2 class="name">Amjad I.</h2>
+                <p class="position">Verified Graduate</p>
+            </div>
+        </div>
+        <p>
+            An excellent introduction in the cryptology world. This training is very good for people which do not have knowledge about cryptocurrencies,
+            but they are willing to learn if they want to invest.
+        </p>
+
+    </article>
+    <article class="testimonial bg-two">
+        <div class="flex">
+            <div>
+                <img  src="./img/image-jonathan.jpg" alt="daniel clifford">
+            </div>
+            <div>
+                <h2 class="name">Massimo T.</h2>
+                <p class="position">Verified Graduate</p>
+            </div>
+        </div>
+        <p>
+            Easy to understand the way of speaking as I am non English native user and
+            the clear outlook which helps me understand the roadmap of the course.
+        </p>
+    </article>
+    <article class="testimonial bg-three">
+        <div class="flex">
+            <div>
+                <img  src="./img/image-patrick.jpg" alt="daniel clifford">
+            </div>
+            <div>
+                <h2 class="name">marco M.</h2>
+                <p class="position">Verified Graduate</p>
+            </div>
+        </div>
+        <p>
+            Amazing teacher , funny , super smart guy .
+            thank you so much for that class i love it . hello from canada brother
+            just buy that class it worth every penny. 100%
+        </p>
+
+    </article>
+    <article class="testimonial bg-four grid-span-2">
+        <div class="flex">
+            <div>
+                <img  src="./img/image-daniel.jpg" alt="daniel clifford">
+            </div>
+            <div>
+                <h2 class="name">Ron H.</h2>
+                <p class="position">Verified Graduate</p>
+            </div>
+        </div>
+        <p>
+            Very professional thorough coverage of cryptocurrency considerations, investing, mining. Bonus material showing
+            how the great spreadsheet was built and the programming logic behind it was very interesting and well explained.
+        </p>
+
+    </article>
+</section>
